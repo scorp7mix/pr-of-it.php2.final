d56dab200f8e94b302514ed2d22a5a0e1dbec2cb
scorp7mix <scorp7mix@gmail.com>
2016-03-10 12:02:32 +0300

    add book model

---
+++ src/AppBundle/Controller/BookController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\Book;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class BookController extends Controller
+{
+    /**
+     * @Route("/book/index", name="book_index")
+     */
+    public function indexAction()
+    {
+        $books = $this->getDoctrine()
+            ->getRepository('AppBundle:Book')
+            ->findAll();
+
+        return $this->render('book/index.html.twig', ['books' => $books]);
+    }
+
+    /**
+     * @Route("/book/show/{id}")
+     */
+    public function showAction(Book $book)
+    {
+        var_dump($book);
+        return new Response('Shown details for book id ' . $book->getId());
+    }
+
+    /**
+     * @Route("/book/create")
+     */
+    public function createAction()
+    {
+        $book = new Book();
+        $book->setTitle('Сталкивающиеся солнца');
+        $book->setOrigin('Crushing Suns');
+        $book->setYear(1928);
+
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $entityManager->persist($book);
+        $entityManager->flush();
+
+        return new Response('Created book id ' . $book->getId());
+    }
+
+    /**
+     * @Route("/book/update/{id}")
+     */
+    public function updateAction(Book $book)
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $book->setYear(1900);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('book_index');
+    }
+
+    /**
+     * @Route("/book/delete/{id}")
+     */
+    public function deleteAction(Book $book)
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $entityManager->remove($book);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('book_index');
+    }
+}
---
+++ src/AppBundle/Entity/Book.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Class Book
+ *
+ * @package AppBundle\Entity
+ *
+ * @ORM\Entity
+ * @ORM\Table(name="book")
+ */
+class Book
+{
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @ORM\Column(type="string", length=100, unique=true)
+     */
+    protected $title;
+
+    /**
+     * @ORM\Column(type="string", length=100, nullable=true)
+     */
+    protected $origin;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    protected $year;
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     *
+     * @return Book
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set origin
+     *
+     * @param string $origin
+     *
+     * @return Book
+     */
+    public function setOrigin($origin)
+    {
+        $this->origin = $origin;
+
+        return $this;
+    }
+
+    /**
+     * Get origin
+     *
+     * @return string
+     */
+    public function getOrigin()
+    {
+        return $this->origin;
+    }
+
+    /**
+     * Set year
+     *
+     * @param integer $year
+     *
+     * @return Book
+     */
+    public function setYear($year)
+    {
+        $this->year = $year;
+
+        return $this;
+    }
+
+    /**
+     * Get year
+     *
+     * @return integer
+     */
+    public function getYear()
+    {
+        return $this->year;
+    }
+}
